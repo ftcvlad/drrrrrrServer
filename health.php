f519030283a046f9e8a2b432c6181a171837feb4
Vladislav <vladislav.voicehovich@gmail.com>
2018-05-03 13:43:27 +0100

    added health.php for openshift

---
+++ health.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vlad
+ * Date: 03/05/2018
+ * Time: 13:42
+ */
+
+echo json_encode(array());//return 200 -- https://docs.openshift.com/enterprise/3.1/dev_guide/application_health.html
